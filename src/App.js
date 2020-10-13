ac627a6a321e4392805c8d77f03688e0340a6baf
Steven Spicher <steven.spicher@gmail.com>
2020-10-13 15:00:10 -0600

    added homepage element

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-
+import HomePage from "./components/home-page/home.jsx"
 import AboutPage from "./components/about-page/about-page.jsx"
 function App() {
   return (
  <AboutPage/>
+
+ <HomePage/>
   );
 }
 
