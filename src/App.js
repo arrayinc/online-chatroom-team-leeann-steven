db0855819928e2674aa3893c36c5eea82676db98
LeeAnn Grapes <leeann.grapes@gmail.com>
2020-10-14 14:09:09 -0600

    Set up router in index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from "./components/home-page/home.jsx"
+import AboutPage from "./components/about-page/about-page.jsx"
+import Navbar from "./components/navbar/navbar.jsx"
+import ChatBox from "./components/chatbox/chatbox"
+
 //import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-//import HomePage from "./components/home-page/home.jsx"
-import AboutPage from "./components/about-page/about-page.jsx"
-//import Navbar from "./components/navbar/navbar.jsx"
+
 function App() {
   return (
-    // <AboutPage/>
-    <ChatBox/>
+    <React.Fragment>
+      <Navbar />
+        <main>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/home' component={HomePage} />
+            <Route path='/about' component={AboutPage} />
+            <Route path='/chat' component={ChatBox} />
+          </Switch>
+        </main>
+    </React.Fragment>
   );
 }
 
