3f9a8d23d16fae3a2cc4beafa5812f07a80d3d46
Steven Spicher <steven.spicher@gmail.com>
2020-10-13 19:42:21 -0600

    about-page update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import HomePage from "./components/home-page/home.jsx"
+// import HomePage from "./components/home-page/home.jsx"
 import AboutPage from "./components/about-page/about-page.jsx"
 function App() {
   return (
--- src/components/about-page/about-page.jsx
+++ src/components/about-page/about-page.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './about-page.css';
 
+import LeeAnn from './images/LeeAnn.jpg'
+import Steven from './images/Steven.png'
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 import Button from 'react-bootstrap/Button';
 
 
@@ -15,24 +18,42 @@ export default class AboutPage extends React.Component {
 
     render() {      
         return (
-            <Container>
-            <Row className="justify-content-md-center">
-              <Col xs lg="2">
-                1 of 3
-              </Col>
-              <Col md="auto">Variable width content Variable width content</Col>
-              <Col xs lg="2">
-                3 of 3
-              </Col>
-            </Row>
-            <Row>
-              <Col>1 of 3</Col>
-              <Col md="auto">Variable content</Col>
-              <Col xs lg="2"> <Card/>
-                3 of 3
-              </Col>
-            </Row>
-          </Container>
+            <div>
+                <Jumbotron>
+                <center><h1>About Us!</h1></center>
+  <p><center>
+   Here's where we will tell you all about the creators of this chatroom.
+    </center></p>
+  
+                </Jumbotron>
+                
+         
+
+          <div>
+              <center><CardDeck>
+          <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src= {LeeAnn} />
+  <Card.Body>
+    <Card.Title>LeeAnn</Card.Title>
+    <Card.Text>
+      Here's some info about LeeAnn!
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src= {Steven} />
+  <Card.Body>
+    <Card.Title>Steven</Card.Title>
+    <Card.Text>
+    Here's some info about Steven!
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+</CardDeck></center>
+          </div>
+          </div>
         )
     }
 }
