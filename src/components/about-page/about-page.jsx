63df6403fbc696fa0da8433217ff9931190d815c
LeeAnn Grapes <leeann.grapes@gmail.com>
2020-10-18 22:14:01 -0600

    Styling changes across all components

--- src/components/about-page/about-page.jsx
+++ src/components/about-page/about-page.jsx
@@ -1,59 +1,55 @@
 import React from 'react';
+import '../../index.css';
 import './about-page.css';
 
-import LeeAnn from './images/LeeAnn.jpg'
-import Steven from './images/Steven.png'
+import Steven from '../../images/steven copy.png'
+import LeeAnn from '../../images/leeann.jpg'
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
-import Button from 'react-bootstrap/Button';
 
+// import Col from 'react-bootstrap/Col';
+// import Button from 'react-bootstrap/Button';
 
 export default class AboutPage extends React.Component {
-
-
     render() {      
         return (
             <div>
-                <Jumbotron>
-                <center><h1>About Us!</h1></center>
-  <p><center>
-   Here's where we will tell you all about the creators of this chatroom.
-    </center></p>
-  
-                </Jumbotron>
-                
-         
-
-          <div>
-              <center><CardDeck>
-          <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src= {LeeAnn} />
-  <Card.Body className="about-card-body">
-    <Card.Title>LeeAnn</Card.Title>
-    <Card.Text>
-      Here's some info about LeeAnn!
-    </Card.Text>
-    
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src= {Steven} />
-  <Card.Body className="about-card-body">
-    <Card.Title>Steven</Card.Title>
-    <Card.Text>
-    Here's some info about Steven!
-    </Card.Text>
-    
-  </Card.Body>
-</Card>
-</CardDeck></center>
-          </div>
-          </div>
+              <Jumbotron className="jumbo">
+                <center><h1>ABOUT US</h1></center>
+                <h4>
+                  We're a small team with big aspirations.
+                </h4>
+              </Jumbotron>
+              
+              <Container fluid>
+              <Row className="d-flex justify-content-center">
+                <CardDeck>
+                  <Card className="text-center card-about">
+                    <Card.Img variant="top" src= {LeeAnn} />
+                    <Card.Body >
+                      <Card.Title><h2>LeeAnn</h2></Card.Title>
+                      <Card.Text>
+                        <p>Click here to meet LeeAnn!</p>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                  <Card className="text-center card-about">
+                    <Card.Img variant="top" src= {Steven} />
+                    <Card.Body>
+                      <Card.Title><h2>Steven</h2></Card.Title>
+                      <Card.Text>
+                        Click here to meet Steven! 
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </CardDeck>
+              </Row>
+              </Container>
+            </div>
         )
     }
 }
--- src/components/chatbox/chatbox.jsx
+++ src/components/chatbox/chatbox.jsx
@@ -1,45 +1,51 @@
 import React from 'react';
+import '../../index.css';
 import './chatbox.css';
 
-
-
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default class ChatBox extends React.Component {
-
-
     render() {      
         return (
             <div>
-                <Jumbotron>
-                <h1>PUBLIC CHATROOM</h1>
+                <Jumbotron className="jumbo">
+                  <h2>Welcome to the</h2>
+                  <h1>PUBLIC CHATROOM</h1>
                 </Jumbotron>
-                <Container>Chat goes here</Container>
-                <Container className= "input-form">
-                <Form>
-                    <Form.Group className controlId="chat-input">
+                
+                <Row>
+                  <Col md="3">
+                    <div className="sidebar" >
                     
-                <Form.Control type="input" placeholder="Enter chat" />
+                    </div>
+                  </Col>
+                  <Col xs="6">
+                    <Container>Chat goes here</Container>
+                    <Container className= "input-form">
+                      <Form>
+                        <Form.Group className controlId="chat-input">
+                          <Form.Control type="input" placeholder="Enter chat" />
+                        </Form.Group> 
+                      </Form> 
+                    </Container>
+                  </Col>
+                  <Col>
+                    <div className="sidebar" >
+                        
+                    </div>
+                  </Col>
+                </Row>
                 
-               
-                
-              </Form.Group> 
-              
-            </Form> 
-            
-            </Container>
-            <div><center><Button type="submit" className="my-1">Submit
-            </Button></center></div>
-         
-
-     
-          </div>
+            <div><center>
+              <Button type="submit" className="my-1">Submit
+              </Button></center></div>
+            </div>
         )
     }
 }
