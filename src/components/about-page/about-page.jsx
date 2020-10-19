1a7426154aca56eb338ff6755198336e170cd8ae
Steven Spicher <steven.spicher@gmail.com>
2020-10-19 09:22:53 -0600

    inital redux setup

--- src/components/about-page/about-page.jsx
+++ src/components/about-page/about-page.jsx
@@ -1,59 +1,61 @@
-import React from 'react';
-import './about-page.css';
+import React from "react";
+import "./about-page.css";
 
-import LeeAnn from './images/LeeAnn.jpg'
-import Steven from './images/Steven.png'
+import LeeAnn from "./images/LeeAnn.jpg";
+import Steven from "./images/Steven.png";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Button from 'react-bootstrap/Button';
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import Button from "react-bootstrap/Button";
 
 export default class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <center>
+            {" "}
+            <h1> About Us! </h1>
+          </center>
+          <p>
+            {" "}
+            <center>
+              Here's where we will tell you all about the creators of this
+              chatroom.{" "}
+            </center>{" "}
+          </p>
+        </Jumbotron>
 
-
-    render() {      
-        return (
-            <div>
-                <Jumbotron>
-                <center><h1>About Us!</h1></center>
-  <p><center>
-   Here's where we will tell you all about the creators of this chatroom.
-    </center></p>
-  
-                </Jumbotron>
-                
-         
-
-          <div>
-              <center><CardDeck>
-          <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src= {LeeAnn} />
-  <Card.Body className="about-card-body">
-    <Card.Title>LeeAnn</Card.Title>
-    <Card.Text>
-      Here's some info about LeeAnn!
-    </Card.Text>
-    
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src= {Steven} />
-  <Card.Body className="about-card-body">
-    <Card.Title>Steven</Card.Title>
-    <Card.Text>
-    Here's some info about Steven!
-    </Card.Text>
-    
-  </Card.Body>
-</Card>
-</CardDeck></center>
-          </div>
-          </div>
-        )
-    }
+        <div>
+          <center>
+            {" "}
+            <CardDeck>
+              <Card
+                style={{
+                  width: "18rem",
+                }}
+              >
+                <Card.Img variant="top" src={LeeAnn} />{" "}
+                <Card.Body className="about-card-body">
+                  <Card.Title> LeeAnn </Card.Title>
+                  <Card.Text>Here 's some info about LeeAnn! </Card.Text>
+                </Card.Body>{" "}
+              </Card>{" "}
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={Steven} />{" "}
+                <Card.Body className="about-card-body">
+                  <Card.Title> Steven </Card.Title>{" "}
+                  <Card.Text>Here 's some info about Steven! </Card.Text>
+                </Card.Body>{" "}
+              </Card>{" "}
+            </CardDeck>
+          </center>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/chatbox/chatbox.jsx
+++ src/components/chatbox/chatbox.jsx
@@ -1,45 +1,66 @@
-import React from 'react';
-import './chatbox.css';
+import React from "react";
+import "./chatbox.css";
+import { createStore, combineReducers } from "redux";
+
+import { configureStore } from "@reduxjs/toolkit";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+let chatEntry
+let chat
+//action 
+const ENTER_CHAT = 'ENTER_CHAT'
+  {
+  type: ENTER_CHAT,     
+  text: 'hi'
+  }
+  
+function addChat(text) {
+  return {
+    type: ENTER_CHAT,
+  }
+}
+
+//reducer
 
 
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+dispatch(addChat(text))
 
 export default class ChatBox extends React.Component {
-
-
-    render() {      
-        return (
-            <div>
-                <Jumbotron>
-                <h1>PUBLIC CHATROOM</h1>
-                </Jumbotron>
-                <Container>Chat goes here</Container>
-                <Container className= "input-form">
-                <Form>
-                    <Form.Group className controlId="chat-input">
-                    
-                <Form.Control type="input" placeholder="Enter chat" />
-                
-               
-                
-              </Form.Group> 
-              
-            </Form> 
-            
-            </Container>
-            <div><center><Button type="submit" className="my-1">Submit
-            </Button></center></div>
-         
-
-     
-          </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <h1>PUBLIC CHATROOM</h1>
+        </Jumbotron>
+        <Container>{chatEntry.payload}</Container>
+        <Container className="input-form">
+          <Form>
+            <Form.Group className controlId="chat-input">
+              <Form.Control
+                type="input"
+                placeholder="Enter chat"
+                value={chatEntry.payload}
+              />
+            </Form.Group>
+          </Form>
+        </Container>
+        <div>
+          <center>
+            <Button
+              type="submit"
+              className="my-1"
+              onClick={chatSubmit(chatEntry.payload)}
+            >
+              Submit
+            </Button>
+          </center>
+        </div>
+      </div>
+    );
+  }
 }
