9a1799f82d8063d4170d578dfc684c58d7c9c260
LeeAnn Grapes <leeann.grapes@gmail.com>
2020-10-20 16:45:16 -0600

    dashboard modifications

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-
+//import { currentUserInfo, currentChannelInfo } from "../../js/features/userInfo/userInfoSlice";
+import { useSelector } from 'react-redux';
 //import { useDispatch } from "react-redux";
-//import { useSelector } from 'react-redux';
-
-import { setUsername, setAvatar, setChannel } from './dashboardSlice';
+//import { setUsername, setAvatar, setChannel } from './dashboardSlice';
 
 export default function Dashboard() {
 
     //const dispatch = useDispatch();
+    //const username = useSelector(currentUserInfo);
+    //const channel = useSelector(currentChannelInfo);
     //const [currentDashboard, setCurrentDashboard] = useState("");
     
     return (
@@ -19,15 +20,15 @@ export default function Dashboard() {
             <div className="user-dashboard">
                 <Row>
                     <Col xs="4">
-                        <h5 className="mr-auto">Current chatroom: {setChannel}</h5>
+                        <h5 className="mr-auto">Current chatroom:</h5>
                     </Col>
                     <Col xs="4">
                     </Col>
                     <Col xs="3">
-                        <h5>Username: {setUsername}</h5>
+                        <h5>Username:</h5>
                     </Col>
                     <Col xs="1">
-                        <img src={setAvatar} alt="avatar"></img>
+                        
                     </Col>
                 </Row>
             </div>
