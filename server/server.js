563deb73a7d342f99ba8e54a1f9d46e787d3afc1
Steven Spicher <steven.spicher@gmail.com>
2020-10-27 14:53:48 -0600

    added server with beep/boop

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+
+// var app = require('express')();
 const express = require('express');
 const app = express();
 const PORT = 3001;
@@ -6,37 +8,41 @@ const PORT = 3001;
 //LG added http, io, router, path, bodyParser, cors consts
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const router = express.Router();
+// const router = express.Router();
 const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const { isObject } = require('util');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const { isObject } = require('util');
 
-app.use(cookieParser());
-app.use(bodyParser.json()); //LG added
-app.use(session({ secret: 'Truly a secret'}));
-app.use(express.urlencoded({extended: true})); //Matt used bodyParser here instead of express
-app.use(cors()); //LG added
+// app.use(cookieParser());
+// app.use(bodyParser.json()); //LG added
+//app.use(session({ secret: 'Truly a secret'}));
+// app.use(express.urlencoded({extended: true})); //Matt used bodyParser here instead of express
+// app.use(cors()); //LG added
 
-app.post('/', (req, res) => {   
-console.log("receiving")
-    res.end();
-});
+// app.post('/', (req, res) => {   
+// console.log("receiving")
+//     res.end();
+// });
 
 app.get('/', (req, res) => {
-    res.send('hi'); //LG testing here to load a page in route
+     res.send('hi'); //LG testing here to load a page in route
     //res.sendFile(path.join(__dirname, "src", "/index.js"));
-    //res.sendFile(__dirname + '/index-test.html');
+    //res.sendFile( path.resolve('public/index.html') );
 });
-
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg) & console.log("message sent");
+    });
+  });
 //LG added io.on from Getting Started project
 io.on('connection', (socket) => {
-    console.log('a user is connected');
+    console.log ('beep');
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+      console.log('boop');
     });
 });
     
---
+++ server/server1.js
@@ -0,0 +1,41 @@
+var app = require("express")();
+const express = require("express")();
+// const app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var port = process.env.PORT || 3001;
+
+//const router = express.Router();
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const { isObject } = require("util");
+
+app.use(cookieParser());
+app.use(bodyParser.json()); //LG added
+app.use(session({ secret: "Truly a secret" }));
+//app.use(express.bodyParser({extended: true})); //Matt used bodyParser here instead of express
+app.use(cors()); //LG added
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg) & console.log("message sent");
+//   });
+// });
+
+io.on("connection", (socket) => {
+  console.log("beep");
+  socket.on("disconnect", () => {
+    console.log("boop");
+  });
+});
+
+http.listen(port, function () {
+  console.log("listening on *:" + port);
+});
