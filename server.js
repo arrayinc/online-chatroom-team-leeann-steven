4007c92f58390be56aba77504df07e410cf29ba2
LeeAnn Grapes <leeann.grapes@gmail.com>
2020-10-28 14:34:27 -0600

    fixed app.get *

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/', function(req, res){
 });
 
 
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -40,7 +41,7 @@ io.on('connection', function(socket){
 //   });
 // });
 
-app.get('*', (req, res) => res.sendFile(path.join(_dirname, 'build', '/public/index.html'))); //added
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', '/public/index.html'))); //added
 
 http.listen(port, function () {
   console.log("listening on *:" + port);
