5f88fbd07e4b8473a1f82f64fb9d9ad5df3cda63
LeeAnn Grapes <leeann.grapes@gmail.com>
2020-10-28 13:52:17 -0600

    edited app.get path

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on('connection', function(socket){
 //   });
 // });
 
-app.get('*', (req, res) => res.sendFile(path.join(_dirname, 'build', '/index.html'))); //added
+app.get('*', (req, res) => res.sendFile(path.join(_dirname, 'build', 'index.html'))); //added
 
 http.listen(port, function () {
   console.log("listening on *:" + port);
