c479f39edd7f75aa0ee4b09b03757bb56387ff94
Steven Spicher <steven.spicher@gmail.com>
2020-10-29 11:43:27 -0600

    text color

--- src/scenes/Chatroom/components/chatdisplay/ChatDisplay.jsx
+++ src/scenes/Chatroom/components/chatdisplay/ChatDisplay.jsx
@@ -14,15 +14,26 @@ export default function ChatDisplay() {
   socket.once("chat message", (msg) => {
     dispatch(messageListReducer(msg));
   });
-
+  var textcolorlimit = 50;
+  const textChange = (r, g, b) => {
+    if (r < textcolorlimit) {
+      var textcolor = "white";
+    } else if (g < textcolorlimit) {
+      var textcolor = "white";
+    } else if (b < textcolorlimit) {
+      var textcolor = "white";
+    }
+    return textcolor;
+  };
   const messagelist = chatMessage.map((chat) => {
     return (
       <ListGroup.Item
         style={{
-          backgroundColor: chat.color,
+          backgroundColor: `rgb(${chat.color.r}, ${chat.color.g}, ${chat.color.b})`,
           margin: "10px",
           borderRadius: "20px",
           width: "auto",
+          color: textChange(chat.color.r, chat.color.g, chat.color.b),
         }}
       >
         {" "}
